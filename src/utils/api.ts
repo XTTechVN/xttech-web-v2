800cb0ab07e1b173ffb75dad7929b6845f445a7d
trvv <trhuvan@gmail.com>
2026-06-01 22:09:52 +0700

    chore: modify api intercepter flow, support auto refresh new access token

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -8,4 +8,49 @@ const api = axios.create({
   withCredentials: true,
 });
 
+// tự động refresh access token khi expired
+api.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
+
+    // bỏ qua những api không cần kiểm tra
+    if (
+      originalRequest.url.includes('/auth/signin') ||
+      originalRequest.url.includes('/auth/signup') ||
+      originalRequest.url.includes('/auth/refresh')
+    ) {
+      return Promise.reject(error);
+    }
+
+    // đặt limit thử lại
+    originalRequest._retryCount = originalRequest._retryCount || 0;
+
+    if (error.status === 401 && originalRequest._retryCount < 3) {
+      originalRequest._retryCount++;
+
+      try {
+        // call api refresh, api này sẽ tự cấp phát vào cookie
+        const response = await axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/refresh`,
+          {},
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            withCredentials: true,
+          },
+        );
+
+        // call lại api đã lỗi với access token mới
+        return api(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
+
 export default api;
