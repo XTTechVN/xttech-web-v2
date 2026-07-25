770d3b14f5af651da235efec9cb95b8099074790
trv <trhuvan@gmail.com>
2026-07-25 16:44:10 +0700

    refactor: remove dashboard modules, strip application layout, and clean up administrative routes

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,50 +1,17 @@
 'use client';
 
-import AppHeader from '@/components/header/AppHeader';
-import BaseHeader from '@/components/header/BaseHeader';
-import AppSidebar from '@/components/sidebar/AppSidebar';
-import BaseSidebar from '@/components/sidebar/BaseSidebar';
-import { SidebarProvider } from '@/contexts/SidebarProvider';
-import LoadingScreen from '@/components/loading/LoadingScreen';
-import SidebarContent from '@/components/sidebar/SidebarContent';
-
 import queryClient from '@/utils/query';
 import { Toaster } from 'react-hot-toast';
+import { SidebarProvider } from '@/contexts/SidebarProvider';
 import { QueryClientProvider } from '@tanstack/react-query';
-import useUserStore from '@/stores/useUserStore';
-import { useEffect, Suspense } from 'react';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  const { user, hasHydrated } = useUserStore();
-
-  useEffect(() => {
-    if (!hasHydrated) return;
-
-    if (!user) {
-      window.location.href = '/signin';
-    }
-  }, [user, hasHydrated]);
-
-  if (!user) return <LoadingScreen animate={false} />;
-
   return (
     <>
       <Toaster position="top-center" />
       <QueryClientProvider client={queryClient}>
         <SidebarProvider>
-          <BaseHeader>
-            <AppHeader />
-          </BaseHeader>
-          <div className="flex">
-            <BaseSidebar>
-              <AppSidebar />
-            </BaseSidebar>
-            <SidebarContent>
-              <Suspense fallback={<LoadingScreen animate={true} />}>
-                {children}
-              </Suspense>
-            </SidebarContent>
-          </div>
+          <div>{children}</div>
         </SidebarProvider>
       </QueryClientProvider>
     </>
--- src/app/app/loading.tsx
+++ src/app/app/loading.tsx
@@ -1,12 +1,3 @@
-import { getUsers } from '@/app/actions';
-import { User } from '@/types';
-
 export default async function Loading() {
-  const users: User[] = await getUsers();
-
-  const userIds = users.map((user) => user.id.trim());
-
-  console.log(userIds);
-
   return <div>Loading...</div>;
 }
