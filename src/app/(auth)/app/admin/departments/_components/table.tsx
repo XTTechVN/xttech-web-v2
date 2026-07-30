241da28f97f336d97bfde9850088beb62d55b490
quyentph <quyentph@gmail.com>
2026-07-30 18:00:24 +0700

    feat: implement departments management page with statistics, action bar, and table components

---
+++ src/app/(auth)/app/admin/departments/_components/table.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Table = () => {
+    return (
+        <div className='p-2 border borrder-gray-300 shadow-sm rounded-lg bg-white'>Table</div>
+    )
+}
+
+export default Table
