26312864d4625ffd9fa327cb0eb4e101f24f2881
quyentph <quyentph@gmail.com>
2026-07-30 12:36:48 +0700

    feat: implement authentication system with Zustand store, Axios interceptors, and root layout configuration

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/stores';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://api_vision.bosky.vn',
+  baseURL:`${process.env.NEXT_PUBLIC_API_URL}/api/v1` || 'https://dev-xt-api-v2.up.railway.app/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
