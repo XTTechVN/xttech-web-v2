d66f8e8e974c4fc8297cb233dcca8096e2073e0a
trv <trhuvan@gmail.com>
2026-08-04 17:29:01 +0700

    fix: clear xt-auth cookie during logout process

--- src/app/(auth)/app/(sidebar)/profile/page.tsx
+++ src/app/(auth)/app/(sidebar)/profile/page.tsx
@@ -11,6 +11,7 @@ import { LogOut } from 'lucide-react';
 const Page = () => {
   const handleLogout = () => {
     localStorage.clear();
+    document.cookie = 'xt-auth=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT;';
     window.location.href = '/signin';
   };
 
